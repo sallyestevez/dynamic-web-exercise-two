0b059e5bc233c0f916f5fee54c38fd4dbf21ff86
Sally E <sme418@nyu.edu>
2022-09-20 09:39:45 -0400

    added React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import Article from "./pages/article";
+import List from './pages/list';
+
+const router = createBrowserRouter([ /* function takes in one argument - array containing objects */
+  {
+    path: "/",
+    element: <List />,
+  },
+  {
+    path: "/article",
+    element: <Article />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App">
-        
-      </header>
+        <RouterProvider router={router} />
     </div>
   );
 }
